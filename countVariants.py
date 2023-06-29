e52b9191146625a63ecaf64c81af7c3e721dadf6
luppala21 <79038420+luppala21@users.noreply.github.com>
2023-06-28 22:24:26 -0500

    Working code upload

--- countVariants.py
+++ countVariants.py
@@ -1,8 +1,6 @@
 # NAME: Lavanya Uppala
 # EMAIL: lvu06382@creighton.edu
 #
-# Partners: NONE
-#
 # The program reads in a .txt file, extracts the appropriate columns,
 # such as the variant type and patient-family identification id
 # and then returns a .csv which summarizes the different mutations by
@@ -16,38 +14,35 @@ import sys
 import getopt
 import re
 import pandas as pd
-import csv
 
 class countVariants:
 
     # outputs usage information such as command line options/arguments
     def usage(self):
-        print("Usage: FindSeq [-i FILE] [-o FILE] [-c CHROMOSOME]")
+        print("Usage: countVariants [-i FILE] [-o FILE]")
         print("\t-i: input file (.txt, tab-delimited); STDIN if not used.")
         print("\t-o: output file (.csv-formatted); STDOUT if not used.")
 
     def main(self):
 
+        # CHANGE THIS LINE TO SPECIFY THE INPUT FILE
+        input_file_name = "vcfFile-long.txt"
+
+        # CHANGE THIS LINE TO SPECIFY THE OUTPUT FILE
+        output_file_name = "variantCounts.csv"
+
         try:
             # reads in arguments from the command line
             opts, args = getopt.getopt(sys.argv[1:], "i:o:c")
             
-            input_file_name = ""
-            output_file_name = ""
+            
 
             # sets values of variables depending on command line arguments
-            for (opt, arg) in opts:
+            for (opt, args) in opts:
                 if opt == "-i":
-                    input_file_name = arg
+                    input_file_name = args
                 elif opt == "-o":
-                    output_file_name = arg
-
-        
-        # CHANGE THIS LINE TO SPECIFY THE INPUT FILE
-        # input_file_name = ""
-
-        # CHANGE THIS LINE TO SPECIFY THE OUTPUT FILE
-        # output_file_name = ""
+                    output_file_name = args
 
 
         # exception handling for invalid options
@@ -57,21 +52,56 @@ class countVariants:
             self.usage()
             sys.exit(2)
 
-        # list which stores all the lines read in from input
-        file_input = []
-
-        # file to be read
-        inFile = ""
-
+        # sets the input location of the program, stdin if not specified
         if input_file_name == "":
             inFile = sys.stdin
         else:
             inFile = open(input_file_name, "r")
 
         # reads in the entirety of the .txt file
-        reader = csv.reader(inFile, delimiter="\t")
-        d = list(reader)
-        print(d)
+        patient_ids = pd.read_csv(input_file_name, delimiter="\t",
+                                        dtype=str, header=(0), lineterminator="\n")
+
+        # this drops the first 8 columns of the vcf file which is
+        # extraneous data that's not needed
+        patient_ids = patient_ids.drop(patient_ids.columns[0:9], axis=1)
+
+        family_id = []
+        countWildtype = []
+        countHetVar = []
+        countHomoVar = []
+
+        for column in patient_ids:
+            wildtypeVar = 0
+            hetVar = 0
+            homoVar = 0
+
+            # this is the list of variants associated with a specific patient
+            for i in range(len(patient_ids[column])):
+
+                # this loop makes a running count of the different
+                # mutation types associated with each patient
+                if patient_ids[column][i] != ".:.:.:.:.:.:.:.":
+                   
+                   # gets the variant type as a string
+                    variantType = patient_ids[column][i][0:3]
+                   
+                    if variantType == "0/0":
+                        wildtypeVar = wildtypeVar + 1
+                    elif variantType == "0/1":
+                        hetVar = hetVar + 1
+                    elif variantType == "1/1":
+                        homoVar = homoVar + 1
+            
+            family_id.append(column.strip())
+            countWildtype.append(wildtypeVar)
+            countHetVar.append(hetVar)
+            countHomoVar.append(homoVar)
+
+        variantCounts = pd.DataFrame({"family_id": family_id,
+                                      "countWildType": countWildtype,
+                                      "countHetVar": countHetVar,
+                                      "countHomoVar": countHomoVar})
 
         # where output is sent, default stdout
         outPut = sys.stdout
@@ -79,8 +109,11 @@ class countVariants:
         # outputs text to screen or file depending on option argument
         if output_file_name != "":
             outPut = open(output_file_name, "w")
+        
+        variantCounts.to_csv(output_file_name, sep=",", encoding="utf-8", index=False)
 
         outPut.close()
+        inFile.close()
 
         
 
